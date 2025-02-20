1d22f3803cc1429d61e58f3c110c7807e814ca96
Yadong Xie <vthinkxie@gmail.com>
2025-02-20 12:23:13 +0800

    chore(web): bump to nextjs 15 (#7)

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,28 +1,16 @@
-import OpenAI from 'openai';
-import { OpenAIStream, StreamingTextResponse } from 'ai';
-
+import {streamText} from 'ai';
 export const runtime = 'edge';
-
-const openai = new OpenAI({
-  // Use the LEPTON_API_TOKEN environment variable
-  apiKey: process.env?.LEPTON_API_TOKEN || "Invalid API Key",
-  baseURL: 'https://mixtral-8x7b.lepton.run/api/v1/',
-});
+import { createOpenAI } from '@ai-sdk/openai';
 
 export async function POST(req: Request) {
-  // Extract the `messages` from the body of the request
   const { messages } = await req.json();
-  
-  // Request the OpenAI API for the response based on the prompt
-  const response = await openai.chat.completions.create({
-    model: 'mixtral-8x7b',
-    stream: true,
-    messages: messages,
+  const model = createOpenAI({
+    apiKey: process.env?.LEPTON_API_TOKEN || "Invalid API Key",
+    baseURL: 'https://mixtral-8x7b.lepton.run/api/v1/',
+  })
+  const result = streamText({
+    model: model('mixtral-8x7b'),
+    messages,
   });
-  
-  // Convert the response into a friendly text-stream
-  const stream = OpenAIStream(response);
-  
-  // Respond with the stream
-  return new StreamingTextResponse(stream);
+  return result.toDataStreamResponse();
 }
